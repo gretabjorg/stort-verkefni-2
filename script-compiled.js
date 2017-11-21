934388486306056c955e41eca22f88e2da3156b6
Thordis Petursdottir <thordisp@Thordiss-MacBook-Pro.local>
2017-11-21 10:24:41 +0100

    Þegar þessu commit er bætt við, þá mun það: Bæta við flokkum af myndböndum og thumbnails með Javascript

--- script-compiled.js
+++ script-compiled.js
@@ -1,3 +1,90 @@
-"use strict";
+'use strict';
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Myndbandaleiga = function () {
+  function Myndbandaleiga() {
+    _classCallCheck(this, Myndbandaleiga);
+  }
+
+  _createClass(Myndbandaleiga, [{
+    key: 'load',
+
+
+    // Hleður inn thumbnails: poster, title, date, undir hverju category fyrir sig.
+    // TODO: Lesa inn videos.json
+    // TODO: Setja link á poster sem hleður inn öðrum glugga => media palyer.
+    // TODO: Bua til videos ut fra videos.json
+
+    value: function load() {
+      this.container = document.querySelector('main');
+
+      var videosNytt = [{ 'poster': './videos/small.png', 'title': 'Lego!', 'date': 1509804047011 }, { 'poster': './videos/bunny.png', 'title': 'Big Bunny', 'date': 1507804047011 }];
+      this.createVideoCategory('Nýleg myndbönd', videosNytt);
+
+      var videosKennsla = [{ 'poster': './videos/small.png', 'title': 'Lego!', 'date': 1509804047011 }, { 'poster': './videos/16-9.png', 'title': 'Prufu myndband', 'date': 1505904047011 }, { 'poster': './videos/16-9.png', 'title': 'Prufu myndband með löngum texta sem fer í tvær línur', 'date': 1504904047011 }];
+      this.createVideoCategory('Kennslumyndbönd', videosKennsla);
+
+      var videosSkemmti = [{ 'poster': './videos/bunny.png', 'title': 'Big Bunny', 'date': 1507804047011 }, { 'poster': './videos/16-9.png', 'title': 'Prufu myndband', 'date': 1505904047011 }, { 'poster': './videos/16-9.png', 'title': 'Prufu myndband með löngum texta sem fer í tvær línur', 'date': 1504904047011 }];
+      this.createVideoCategory('Skemmtimyndbönd', videosSkemmti);
+    }
+
+    // Býr til category elements í HTML.
+
+  }, {
+    key: 'createVideoCategory',
+    value: function createVideoCategory(title, videos) {
+      var categoryDiv = document.createElement('div');
+      var category = document.createElement('h2');
+
+      categoryDiv.className = 'video__category';
+      category.textContent = title;
+
+      categoryDiv.appendChild(category);
+
+      for (var key in videos) {
+        var video = videos[key];
+        categoryDiv.appendChild(this.createPoster(video.poster, video.title, video.date));
+      }
+
+      this.container.appendChild(categoryDiv);
+    }
+
+    // Býr til poster elements i HTML.
+
+  }, {
+    key: 'createPoster',
+    value: function createPoster(image, title, date) {
+      var videoPoster = document.createElement('div');
+      var posterImg = document.createElement('img');
+      var videoTitle = document.createElement('span');
+      var videoDate = document.createElement('span');
+
+      videoPoster.className = 'video__poster';
+      posterImg.className = 'poster__img';
+      videoTitle.className = 'video__title';
+      videoDate.className = 'video__date';
+
+      posterImg.src = image;
+      videoTitle.textContent = title;
+      videoDate.textContent = date;
+
+      videoPoster.appendChild(posterImg);
+      videoPoster.appendChild(videoTitle);
+      videoPoster.appendChild(videoDate);
+
+      return videoPoster;
+    }
+  }]);
+
+  return Myndbandaleiga;
+}();
+
+document.addEventListener('DOMContentLoaded', function () {
+  var myndbond = new Myndbandaleiga();
+  myndbond.load();
+});
 
 //# sourceMappingURL=script-compiled.js.map
---
+++ script.js
@@ -0,0 +1,79 @@
+class Myndbandaleiga {
+
+  // Hleður inn thumbnails: poster, title, date, undir hverju category fyrir sig.
+  // TODO: Lesa inn videos.json
+  // TODO: Setja link á poster sem hleður inn öðrum glugga => media palyer.
+  // TODO: Bua til videos ut fra videos.json
+
+  load() {
+    this.container = document.querySelector('main');
+
+    const videosNytt = [
+      { 'poster': './videos/small.png', 'title': 'Lego!', 'date': 1509804047011},
+      { 'poster': './videos/bunny.png', 'title': 'Big Bunny', 'date': 1507804047011}
+    ];
+    this.createVideoCategory('Nýleg myndbönd', videosNytt);
+
+    const videosKennsla = [
+      { 'poster': './videos/small.png', 'title': 'Lego!', 'date': 1509804047011},
+      { 'poster': './videos/16-9.png', 'title': 'Prufu myndband', 'date': 1505904047011},
+      { 'poster': './videos/16-9.png', 'title': 'Prufu myndband með löngum texta sem fer í tvær línur', 'date': 1504904047011}
+    ];
+    this.createVideoCategory('Kennslumyndbönd', videosKennsla);
+
+    const videosSkemmti = [
+      { 'poster': './videos/bunny.png', 'title': 'Big Bunny', 'date': 1507804047011},
+      { 'poster': './videos/16-9.png', 'title': 'Prufu myndband', 'date': 1505904047011},
+      { 'poster': './videos/16-9.png', 'title': 'Prufu myndband með löngum texta sem fer í tvær línur', 'date': 1504904047011}
+    ];
+    this.createVideoCategory('Skemmtimyndbönd', videosSkemmti);
+  }
+
+  // Býr til category elements í HTML.
+
+  createVideoCategory(title, videos) {
+    const categoryDiv = document.createElement('div');
+    const category = document.createElement('h2');
+
+    categoryDiv.className = 'video__category';
+    category.textContent = title;
+
+    categoryDiv.appendChild(category);
+
+    for (const key in videos) {
+      const video = videos[key];
+      categoryDiv.appendChild(this.createPoster(video.poster, video.title, video.date));
+    }
+
+    this.container.appendChild(categoryDiv);
+  }
+
+  // Býr til poster elements i HTML.
+
+  createPoster(image, title, date) {
+    const videoPoster = document.createElement('div');
+    const posterImg = document.createElement('img');
+    const videoTitle = document.createElement('span');
+    const videoDate = document.createElement('span');
+
+    videoPoster.className = 'video__poster';
+    posterImg.className = 'poster__img';
+    videoTitle.className = 'video__title';
+    videoDate.className = 'video__date';
+
+    posterImg.src = image;
+    videoTitle.textContent = title;
+    videoDate.textContent = date;
+
+    videoPoster.appendChild(posterImg);
+    videoPoster.appendChild(videoTitle);
+    videoPoster.appendChild(videoDate);
+
+    return videoPoster;
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const myndbond = new Myndbandaleiga();
+  myndbond.load();
+});
