49cefa65af9016f6060062afd4a5e9d65bb7222f
Jullira <32617407+Jullira@users.noreply.github.com>
2017-11-28 16:21:29 +0000

    hef latid alla takka virka

--- video-compiled.js
+++ video-compiled.js
@@ -37,26 +37,53 @@ var MyndbandaSyning = function () {
       request.send();
     }
   }, {
+    key: 'muteVideo',
+    value: function muteVideo() {
+      this.videoElement.muted = true;
+    }
+  }, {
+    key: 'unmuteVideo',
+    value: function unmuteVideo() {
+      this.videoElement.muted = false;
+    }
+  }, {
     key: 'playVideo',
     value: function playVideo() {
-      if (this.videoElement.currentTime > 0 && this.videoElement.paused === false && this.videoElement.ended === false) {
-        this.videoElement.pause();
-        this.videoOverlay.className = 'video__overlay';
-        this.objPlay.className = 'play__button';
-      } else {
-        this.videoElement.play();
-        this.videoOverlay.className = 'video__overlay video__overlay__invisible ';
-        this.objPlay.className = 'play__button invisible';
+      this.videoElement.play();
+      this.videoOverlay.className = 'video__overlay video__overlay__invisible ';
+      this.objPlay.className = 'play__button invisible';
+    }
+  }, {
+    key: 'pauseVideo',
+    value: function pauseVideo() {
+      this.videoElement.pause();
+      this.videoOverlay.className = 'video__overlay';
+      this.objPlay.className = 'play__button';
+    }
+  }, {
+    key: 'fullscreenVideo',
+    value: function fullscreenVideo() {
+      if (this.videoElement.requestFullscreen) {
+        this.videoElement.requestFullscreen();
+      } else if (this.videoElement.mozRequestFullscreen) {
+        this.videoElement.mozRequestFullscreen();
+      } else if (this.videoElement.webkitRequestFullscreen) {
+        this.videoElement.webkitRequestFullscreen();
       }
     }
   }, {
-    key: 'hideDiv',
-    value: function hideDiv(div) {
-      div.classList.add('invisible');
+    key: 'nextVideo',
+    value: function nextVideo() {
+      this.videoElement.currentTime = this.videoElement.currentTime + 3;
+    }
+  }, {
+    key: 'backVideo',
+    value: function backVideo() {
+      this.videoElement.currentTime = this.videoElement.currentTime - 3;
     }
   }, {
     key: 'createPlayer',
-    value: function createPlayer(title) {
+    value: function createPlayer() {
       if (this.video) {
         // Teikna upp video player med HTML sem er skilgreint nu thegar i videos.html
         this.videoContainer = document.createElement('div');
@@ -73,8 +100,6 @@ var MyndbandaSyning = function () {
 
         this.videoTitle.textContent = this.video.title;
 
-        this.videoOverlay.addEventListener('click', this.playVideo.bind(this));
-
         this.videoContainer.appendChild(this.videoTitle);
         this.videoOverlay.appendChild(this.videoElement);
         this.videoElement.src = this.video.video;
@@ -85,33 +110,33 @@ var MyndbandaSyning = function () {
         var objBack = document.createElement('img');
         var objPause = document.createElement('img');
         this.objPlay = document.createElement('img');
-        var objUnmute = document.createElement('img');
         var objMute = document.createElement('img');
+        var objUnmute = document.createElement('img');
         var objFullscreen = document.createElement('img');
         var objNext = document.createElement('img');
 
         objectDiv.className = 'valmynd';
-        objBack.className = 'imgValmynd';
-        objPause.className = 'imgValmynd';
+        objBack.className = 'imgValmynd back__button';
+        objPause.className = 'imgValmynd pause__button';
         this.objPlay.className = 'play__button';
-        objUnmute.className = 'imgValmynd';
-        objMute.className = 'imgValmynd';
-        objFullscreen.className = 'imgValmynd';
-        objNext.className = 'imgValmynd';
+        objMute.className = 'imgValmynd mute__button';
+        objUnmute.className = 'imgValmynd unmute__button';
+        objFullscreen.className = 'imgValmynd fullscreen__button';
+        objNext.className = 'imgValmynd next__button';
 
         objBack.id = 'back__button';
         objPause.id = 'pause__button';
         this.objPlay.id = 'play__button';
-        objUnmute.id = 'unmute__button';
         objMute.id = 'mute__button';
+        objUnmute.id = 'unmute__button';
         objFullscreen.id = 'fullscreen__button';
         objNext.id = 'next__button';
 
         objBack.src = 'img/back.svg';
         objPause.src = 'img/pause.svg';
         this.objPlay.src = 'img/play.svg';
-        objUnmute.src = 'img/unmute.svg';
-        objMute.src = 'img/mute.svg';
+        objMute.src = 'img/unmute.svg';
+        objUnmute.src = 'img/mute.svg';
         objFullscreen.src = 'img/fullscreen.svg';
         objNext.src = 'img/next.svg';
 
@@ -122,8 +147,6 @@ var MyndbandaSyning = function () {
         objectDiv.appendChild(objMute);
         objectDiv.appendChild(objFullscreen);
         objectDiv.appendChild(objNext);
-
-        this.videoElement.pause();
 
         this.container.appendChild(this.videoContainer);
         this.container.appendChild(objectDiv);
@@ -149,6 +172,14 @@ var MyndbandaSyning = function () {
       tilbakaDiv.appendChild(linkur);
 
       this.container.appendChild(tilbakaDiv);
+
+      this.videoOverlay.addEventListener('click', this.playVideo.bind(this));
+      document.getElementById('pause__button').addEventListener('click', this.pauseVideo.bind(this));
+      document.getElementById('mute__button').addEventListener('click', this.muteVideo.bind(this));
+      document.getElementById('unmute__button').addEventListener('click', this.unmuteVideo.bind(this));
+      document.getElementById('fullscreen__button').addEventListener('click', this.fullscreenVideo.bind(this));
+      document.getElementById('next__button').addEventListener('click', this.nextVideo.bind(this));
+      document.getElementById('back__button').addEventListener('click', this.backVideo.bind(this));
     }
   }, {
     key: 'parseVideosJson',
--- video.js
+++ video.js
@@ -25,138 +25,162 @@ class MyndbandaSyning {
     request.send();
   }
 
+  muteVideo() {
+    this.videoElement.muted = true;
+  }
+
+  unmuteVideo() {
+    this.videoElement.muted = false;
+  }
+
   playVideo() {
-    if (this.videoElement.currentTime > 0 && this.videoElement.paused === false &&
-      this.videoElement.ended === false) {
-        this.videoElement.pause();
-        this.videoOverlay.className = 'video__overlay';
-        this.objPlay.className = 'play__button';
-      } else {
-        this.videoElement.play();
-        this.videoOverlay.className = 'video__overlay video__overlay__invisible ';
-        this.objPlay.className = 'play__button invisible';
-      }
-    }
+    this.videoElement.play();
+    this.videoOverlay.className = 'video__overlay video__overlay__invisible ';
+    this.objPlay.className = 'play__button invisible';
+  }
 
-    hideDiv(div) {
-      div.classList.add('invisible');
-    }
+  pauseVideo() {
+    this.videoElement.pause();
+    this.videoOverlay.className = 'video__overlay';
+    this.objPlay.className = 'play__button';
+  }
 
-    createPlayer(title) {
-      if (this.video) {
-        // Teikna upp video player med HTML sem er skilgreint nu thegar i videos.html
-        this.videoContainer = document.createElement('div');
-        this.videoTitle = document.createElement('h2');
-        this.videoOverlay = document.createElement('div');
-        this.videoPlayer = document.createElement('div');
-        this.videoElement = document.createElement('video');
-
-        this.videoContainer.className = 'video__container';
-        this.videoTitle.className = 'video__title';
-        this.videoElement.className = 'video';
-        this.videoOverlay.className = 'video__overlay';
-        this.videoPlayer.className = 'video__player';
-
-        this.videoTitle.textContent = this.video.title;
-
-        this.videoOverlay.addEventListener('click', this.playVideo.bind(this));
-
-        this.videoContainer.appendChild(this.videoTitle);
-        this.videoOverlay.appendChild(this.videoElement);
-        this.videoElement.src = this.video.video;
-        this.videoPlayer.appendChild(this.videoOverlay);
-        this.videoContainer.appendChild(this.videoPlayer);
-
-
-        const objectDiv = document.createElement('div');
-        const objBack = document.createElement('img');
-        const objPause = document.createElement('img');
-        this.objPlay = document.createElement('img');
-        const objUnmute = document.createElement('img');
-        const objMute = document.createElement('img');
-        const objFullscreen = document.createElement('img');
-        const objNext = document.createElement('img');
-
-        objectDiv.className = 'valmynd';
-        objBack.className = 'imgValmynd';
-        objPause.className = 'imgValmynd';
-        this.objPlay.className = 'play__button';
-        objUnmute.className = 'imgValmynd';
-        objMute.className = 'imgValmynd';
-        objFullscreen.className = 'imgValmynd';
-        objNext.className = 'imgValmynd';
-
-        objBack.id = 'back__button';
-        objPause.id = 'pause__button';
-        this.objPlay.id = 'play__button';
-        objUnmute.id = 'unmute__button';
-        objMute.id = 'mute__button';
-        objFullscreen.id = 'fullscreen__button';
-        objNext.id = 'next__button';
-
-        objBack.src = 'img/back.svg';
-        objPause.src = 'img/pause.svg';
-        this.objPlay.src = 'img/play.svg';
-        objUnmute.src = 'img/unmute.svg';
-        objMute.src = 'img/mute.svg';
-        objFullscreen.src = 'img/fullscreen.svg';
-        objNext.src = 'img/next.svg';
-
-        objectDiv.appendChild(objBack);
-        objectDiv.appendChild(objPause);
-        this.videoOverlay.appendChild(this.objPlay);
-        objectDiv.appendChild(objUnmute);
-        objectDiv.appendChild(objMute);
-        objectDiv.appendChild(objFullscreen);
-        objectDiv.appendChild(objNext);
-
-        this.videoElement.pause();
-
-        
-        this.container.appendChild(this.videoContainer);
-        this.container.appendChild(objectDiv);
-      } else {
-        const errorContainer = document.createElement('div');
-        const errorText = document.createElement('p');
-
-        errorContainer.className = 'error';
-
-        errorText.innerHTML = 'Myndband fannst ekki';
-
-        errorContainer.appendChild(errorText);
-
-        this.container.appendChild(errorContainer);
-      }
-      const tilbakaDiv = document.createElement('div');
-      const linkur = document.createElement('a');
-
-      tilbakaDiv.className = 'TilBaka';
-
-      linkur.setAttribute('href', 'index.html');
-      linkur.innerHTML = 'Til baka';
-      tilbakaDiv.appendChild(linkur);
-
-      this.container.appendChild(tilbakaDiv);
-    }
-
-    parseVideosJson(e) {
-      const request = e.target;
-      if (request.status >= 200 && request.status < 400) {
-        const data = JSON.parse(request.response);
-        this.playVideos = data.playVideos;
-
-        // Geymum video i this.videos array
-        for (const videoKey in data.videos) {
-          if (data.videos[videoKey].id === this.videoId) {
-            this.video = data.videos[videoKey];
-          }
-        }
-      }
-      this.createPlayer();
+  fullscreenVideo() {
+    if (this.videoElement.requestFullscreen) {
+      this.videoElement.requestFullscreen();
+    } else if (this.videoElement.mozRequestFullscreen) {
+      this.videoElement.mozRequestFullscreen();
+    } else if (this.videoElement.webkitRequestFullscreen) {
+      this.videoElement.webkitRequestFullscreen();
     }
   }
 
-  document.addEventListener('DOMContentLoaded', () => {
-    const myndbond = new MyndbandaSyning();
-    myndbond.load();
-  });
+  nextVideo() {
+    this.videoElement.currentTime = this.videoElement.currentTime + 3;
+  }
+
+  backVideo() {
+    this.videoElement.currentTime = this.videoElement.currentTime - 3;
+  }
+
+  createPlayer() {
+    if (this.video) {
+      // Teikna upp video player med HTML sem er skilgreint nu thegar i videos.html
+      this.videoContainer = document.createElement('div');
+      this.videoTitle = document.createElement('h2');
+      this.videoOverlay = document.createElement('div');
+      this.videoPlayer = document.createElement('div');
+      this.videoElement = document.createElement('video');
+
+      this.videoContainer.className = 'video__container';
+      this.videoTitle.className = 'video__title';
+      this.videoElement.className = 'video';
+      this.videoOverlay.className = 'video__overlay';
+      this.videoPlayer.className = 'video__player';
+
+      this.videoTitle.textContent = this.video.title;
+
+      this.videoContainer.appendChild(this.videoTitle);
+      this.videoOverlay.appendChild(this.videoElement);
+      this.videoElement.src = this.video.video;
+      this.videoPlayer.appendChild(this.videoOverlay);
+      this.videoContainer.appendChild(this.videoPlayer);
+
+
+      const objectDiv = document.createElement('div');
+      const objBack = document.createElement('img');
+      const objPause = document.createElement('img');
+      this.objPlay = document.createElement('img');
+      const objMute = document.createElement('img');
+      const objUnmute = document.createElement('img');
+      const objFullscreen = document.createElement('img');
+      const objNext = document.createElement('img');
+
+      objectDiv.className = 'valmynd';
+      objBack.className = 'imgValmynd back__button';
+      objPause.className = 'imgValmynd pause__button';
+      this.objPlay.className = 'play__button';
+      objMute.className = 'imgValmynd mute__button';
+      objUnmute.className = 'imgValmynd unmute__button';
+      objFullscreen.className = 'imgValmynd fullscreen__button';
+      objNext.className = 'imgValmynd next__button';
+
+      objBack.id = 'back__button';
+      objPause.id = 'pause__button';
+      this.objPlay.id = 'play__button';
+      objMute.id = 'mute__button';
+      objUnmute.id = 'unmute__button';
+      objFullscreen.id = 'fullscreen__button';
+      objNext.id = 'next__button';
+
+      objBack.src = 'img/back.svg';
+      objPause.src = 'img/pause.svg';
+      this.objPlay.src = 'img/play.svg';
+      objMute.src = 'img/unmute.svg';
+      objUnmute.src = 'img/mute.svg';
+      objFullscreen.src = 'img/fullscreen.svg';
+      objNext.src = 'img/next.svg';
+
+      objectDiv.appendChild(objBack);
+      objectDiv.appendChild(objPause);
+      this.videoOverlay.appendChild(this.objPlay);
+      objectDiv.appendChild(objUnmute);
+      objectDiv.appendChild(objMute);
+      objectDiv.appendChild(objFullscreen);
+      objectDiv.appendChild(objNext);
+
+      this.container.appendChild(this.videoContainer);
+      this.container.appendChild(objectDiv);
+    } else {
+      const errorContainer = document.createElement('div');
+      const errorText = document.createElement('p');
+
+      errorContainer.className = 'error';
+
+      errorText.innerHTML = 'Myndband fannst ekki';
+
+      errorContainer.appendChild(errorText);
+
+      this.container.appendChild(errorContainer);
+    }
+    const tilbakaDiv = document.createElement('div');
+    const linkur = document.createElement('a');
+
+    tilbakaDiv.className = 'TilBaka';
+
+    linkur.setAttribute('href', 'index.html');
+    linkur.innerHTML = 'Til baka';
+    tilbakaDiv.appendChild(linkur);
+
+    this.container.appendChild(tilbakaDiv);
+
+    this.videoOverlay.addEventListener('click', this.playVideo.bind(this));
+    document.getElementById('pause__button').addEventListener('click', this.pauseVideo.bind(this));
+    document.getElementById('mute__button').addEventListener('click', this.muteVideo.bind(this));
+    document.getElementById('unmute__button').addEventListener('click', this.unmuteVideo.bind(this));
+    document.getElementById('fullscreen__button').addEventListener('click', this.fullscreenVideo.bind(this));
+    document.getElementById('next__button').addEventListener('click', this.nextVideo.bind(this));
+    document.getElementById('back__button').addEventListener('click', this.backVideo.bind(this));
+}
+
+  parseVideosJson(e) {
+    const request = e.target;
+    if (request.status >= 200 && request.status < 400) {
+      const data = JSON.parse(request.response);
+      this.playVideos = data.playVideos;
+
+      // Geymum video i this.videos array
+      for (const videoKey in data.videos) {
+        if (data.videos[videoKey].id === this.videoId) {
+          this.video = data.videos[videoKey];
+        }
+      }
+    }
+    this.createPlayer();
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const myndbond = new MyndbandaSyning();
+  myndbond.load();
+});
